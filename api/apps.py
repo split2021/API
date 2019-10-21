ace112b72109cf1d10a445964dc28a7162e518b4
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2019-10-21 16:56:19 -0700

    Add create_hstore on pre_migrate signal

--- api/apps.py
+++ api/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
+from django.db.models.signals import pre_migrate
 
+
+def create_hstore(sender, **kwargs):
+    cursor = connection.cursor()
+    cursor.execute("CREATE EXTENSION IF NOT EXISTS hstore")
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+    def ready(self):
+        pre_migrate.connect(create_hstore, sender=self)
