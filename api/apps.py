24db7edd36ee25c3e782dde780c3a5691ff6435d
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2019-10-21 18:51:11 -0700

    Add HStore to migrations

--- api/apps.py
+++ api/apps.py
@@ -1,12 +1,5 @@
 from django.apps import AppConfig
 
-from django.db import connection
-
 
 class ApiConfig(AppConfig):
     name = 'api'
-
-    def ready(self):
-        print("Create HSTORE")
-        with connection.cursor() as cursor:
-            cursor.execute("CREATE EXTENSION IF NOT EXISTS hstore")
