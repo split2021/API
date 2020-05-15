fce2b93ddd211e9cb0bef25c0e127636d8e99987
Tiphaine LAURENT <tiphaine.laurent@epitech.eu>
2020-05-15 19:13:00 +0200

    Add automatic payout when payment is completed

--- api/admin.py
+++ api/admin.py
@@ -123,3 +123,4 @@ class LogAdmin(admin.ModelAdmin):
 class PaymentAdmin(admin.ModelAdmin):
     """
     """
+    list_display = ('group', 'total', 'currency', 'target', 'is_complete', 'is_failed', 'calculate_payout_price')
